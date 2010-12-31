a81e0a60f1c5292cbe6adb8eea857578b5b97d04
Bradley Herman <bradley.t.herman@gmail.com>
2010-12-30 22:31:25 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Howasit::Application.config.secret_token = '9e409b167ce09fea47142830aac04d02fe68d2397a29be726f2db386c36e67219150f632943d20f805e96f52a65b87ce8aba709dc234c12a02fe24f6333d0cd8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Howasit::Application.config.session_store :cookie_store, :key => '_howasit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Howasit::Application.config.session_store :active_record_store
