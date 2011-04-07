9e6b0d9bca0afac570f42fc4ff7a5994ad616701
Bradley Herman <bradley.t.herman@gmail.com>
2011-04-06 22:51:41 -0400

    Added long/lat

--- app/models/business.rb
+++ app/models/business.rb
@@ -3,5 +3,21 @@ class Business < ActiveRecord::Base
   has_one     :coupon
   has_one     :survey
   
+  geocoded_by :address
+  
+  after_validation :geocode
+  
   validates_presence_of :name, :phone, :street, :city, :state, :zip
+  
+  def address
+    [street, city, state, zip].compact.join(', ')
+  end
+  
+  def survey
+    if survey_id
+      Survey.find(survey_id)
+    else
+      nil
+    end
+  end
 end
